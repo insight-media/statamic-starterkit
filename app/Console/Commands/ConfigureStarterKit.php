4af542fd033d80a35f76576a7b3018d669154328
Dries <dries@insight-media.be>
2022-02-17 19:22:28 +0100

    Add command to configure starter kit.

---
+++ app/Console/Commands/ConfigureStarterKit.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ConfigureStarterKit extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'insightmedia:configure:starterkit';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Additional configuration for the Statamic Insight Media Starter Kit';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        if (config('app.env') == "local")
+        {
+            $this->rename(base_path('public'), base_path('www'));
+            $this->rename(base_path('.env.example'), base_path('.env.master'));
+        }
+
+        return 0;
+    }
+
+    private function rename($from, $to)
+    {
+
+        if (!file_exists($to) && file_exists($from)) {
+            rename($from, $to);
+            $this->info("Renamed $from to $to");
+        }
+    }
+
+}
