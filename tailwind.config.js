ccf8d82909ec8c3044ab87f0c709095f786b5a2e
Dries <dries@insight-media.be>
2022-02-27 12:45:40 +0100

    Enable JIT

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    mode: 'jit',
+    content: [
+        './resources/**/*.antlers.html',
+        './resources/**/*.blade.php',
+        './resources/**/*.vue',
+        './content/**/*.md'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
