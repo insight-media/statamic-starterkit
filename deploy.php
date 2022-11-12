0def2dc862fc35c469412bf788c583e460df8998
Dries <dries@insight-media.be>
2022-11-12 14:15:35 +0100

    Update deploy script

--- deploy.php
+++ deploy.php
@@ -16,6 +16,7 @@ set('repository', $repository);
 set('writable_mode', 'chmod');
 set('update_code_strategy', 'clone');
 set('current_branch', trim(implode('/', array_slice(explode('/', file_get_contents('.git/HEAD')), 2))));
+set('ssh_multiplexing', false);
 
 add('shared_files', []);
 add('shared_dirs', []);
@@ -36,6 +37,24 @@ task('set-origin', function () {
     run('git remote set-url origin '.get('repository'));
 });
 
+desc('Build assets');
+task('build-assets', function () {
+    cd('{{release_or_current_path}}');
+    run('yarn && yarn build');
+    run('chmod -R 775 ./public/build');
+});
+
+desc('Symlink public folder to new release');
+task('symlink-public', function () {
+    run('rm -f -r ~/www');
+    run('ln -s {{release_path}}/public/ ~/www');
+});
+
+desc('Reload PHP service and clear opcache');
+task('reload-php', function () {
+    run('reloadPHP.sh');
+});
+
 desc('Clear cache');
 task('clear-cache', function () {
     cd('{{release_or_current_path}}');
@@ -44,30 +63,25 @@ task('clear-cache', function () {
     run('php please stache:clear');
 });
 
-desc('Symlink public folder to new release');
-task('symlink-public', function () {
-    run('rm ~/www');
-    run('ln -s {{release_path}}/public/ ~/www');
-});
-
 desc('Commit and push current branch on remote host to origin repository');
 task('git-commit', function () {
     cd('{{release_or_current_path}}');
     run('php please git:commit');
 });
 
-desc('Upload .env.{{current_branch}} to host');
-task('upload-env', function () {
-    upload('.env.'.get('current_branch'), get('deploy_path').'/shared/.env');
-});
-
 desc('Configure remote server');
 task('configure-remote', function () {
     // Copy git binary to user account
+    run('mkdir ~/bin');
     run('cp -n /bin/git ~/bin/git');
 
     // Add cronjob
-    run("grep '/please git:commit' /etc/crontab || echo '* * * * * php $HOME/current/please git:commit >/dev/null 2>&1' >> $HOME/.crontab");
+    run("grep '/please git:commit' /etc/crontab || echo '* * * * * php \$HOME/current/please git:commit >/dev/null 2>&1' >> \$HOME/.crontab");
+});
+
+desc('Upload .env.{{current_branch}} to host');
+task('upload-env', function () {
+    upload('.env.'.get('current_branch'), get('deploy_path').'/shared/.env');
 });
 
 desc('Download public storage folder');
@@ -76,6 +90,8 @@ task('download-public-storage', function () {
 });
 
 after('deploy', 'set-origin');
-after('deploy', 'clear-cache');
+after('deploy', 'build-assets');
 after('deploy', 'symlink-public');
+after('deploy', 'reload-php');
+after('deploy', 'clear-cache');
 after('deploy:failed', 'deploy:unlock');
